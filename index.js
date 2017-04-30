584d7a4587fd23d65618e437209894717d18a704
John Desbois <jdesbois@me.com>
2017-04-30 21:23:50 +0100

    added some button functionaility

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var entry = '';
 var log = '';
+var ac = '';
 
 var buttons = document.getElementsByTagName('button');
-var history = document.getElementById('history');
 var input = document.getElementById('input');
+var logText = document.getElementById('history');
 
 for (var i=0; i < buttons.length; i++) {
     buttons[i].onclick = function() {
         entry = this.value;
+        log += this.value;
         input.innerHTML = entry;
-        console.log(entry);
+        logText.innerHTML = log;
     }
 }
