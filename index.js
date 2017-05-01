3a0f6ec99cd9410cf2bb3c8d0c3b10a2ee9ff82c
John Desbois <jdesbois@me.com>
2017-05-01 11:04:48 +0100

    added comments

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function allClear() {
     sumHistory.innerHTML = log;
     input.innerHTML = entry;   
 }
+//EVALUATES LOG STRING
 function evaluate() {
     log = eval(log);
     entry = eval(log);
@@ -23,17 +24,19 @@ function evaluate() {
 }
 
 
-//TEST BUTTONS
+//BUTTONS LOOP
 for (var i=0; i<buttons.length; i++) {
+//ADDING CLICK LISTENER
   buttons[i].addEventListener('click', function(button){  
-
+    //IF EQUALS RUN EVALUATE FUNCTION
     if (this.value == "=") {
         return evaluate();
     }
+    //IF AC RUN allClear FUNCTION
     if (this.value == "AC") {
        return allClear();
     }
-           
+         //TAKE VALUES OF BUTTONS AND ADD TO DOM AND STRINGS  
             log += this.value;
             entry = this.value;
             input.innerHTML = entry;
